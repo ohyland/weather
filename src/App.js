03e926f165b1995307c5f4155d1e68495e675ea8
ohyland <1oliviahyland@gmail.com>
2020-11-26 21:41:48 +0000

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Check the Weather...</h1>
-    </div>
-  );
+  const [weather, setWeather] = useState([]);
+  
+	useEffect(() => {
+		axios
+			.get(
+				"http://api.weatherapi.com/v1/current.json?key=76cb3407f2014f49902211656202611&q=Dublin"
+			)
+			.then((data) => {
+				setWeather(data.data);
+				console.log(data.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<div className="App">
+			<h1>Check the Weather in {weather.location.country}</h1>
+		</div>
+	);
 }
 
 export default App;
