7cad63dc0b83e4ed75b6423c745d01c5a3de2824
ohyland <1oliviahyland@gmail.com>
2020-12-07 21:27:44 +0000

    Show weather data and search input

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [weather, setWeather] = useState([]);
-  
+	const [weather, setWeather] = useState(null); // null -> false until the data is successfully fetched
+	const [weatherInput, setWeatherInput] = useState(""); // null -> false until the data is successfully fetched
+
 	useEffect(() => {
 		axios
 			.get(
@@ -12,13 +13,41 @@ function App() {
 			)
 			.then((data) => {
 				setWeather(data.data);
-				console.log(data.data);
+				console.log(data.data.current);
 			})
 			.catch((err) => console.log(err));
 	}, []);
+	// Event
+	const weatherInputHandler = (e) => {
+		setWeatherInput(e.target.value);
+	};
+
+	const searchWeather = (e) => {
+		axios
+			.get(
+				`http://api.weatherapi.com/v1/current.json?key=76cb3407f2014f49902211656202611&q=${weatherInput}`
+			)
+			.then((data) => {
+				setWeather(data.data);
+			});
+	};
 	return (
 		<div className="App">
-			<h1>Check the Weather in {weather.location.country}</h1>
+			{weather && (
+				<div>
+					<input onChange={weatherInputHandler} type="text" />
+					<button onClick={searchWeather}>Submit</button>
+					<h1>
+						The Weather in {weather.location.region}, {weather.location.country}
+					</h1>
+					<h2>
+						The temperature is {weather.current.temp_c}
+						&#176;c and it feels like {weather.current.feelslike_c}&#176;c
+					</h2>
+					<h2>{weather.current.condition.text}</h2>
+					<img src={weather.current.condition.icon} alt="current condition" />
+				</div>
+			)}
 		</div>
 	);
 }
